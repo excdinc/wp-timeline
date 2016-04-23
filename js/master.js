ddda9df119e9a3eee73b7651d6b25f97d61ce80b
Takeshi Kashihara <kashihara_ybb@yahoo.co.jp>
2016-04-23 15:43:42 +0900

    no message

--- js/master.js
+++ js/master.js
@@ -1,23 +1,18 @@
 var container;
-var camera, scene, renderer;
+var camera, scene, renderer,scene2, renderer2;
 var controls;
 var stats;
 var objects = [];
-var targets = { table: [], sphere: [], helix: [], grid: [] };
 var json;
 
 var yearLength = 1000;
-var opDuration = 30000
+var opDuration = 30000;
 
 var wpXLength = 200;
-
 var yRamdom = 300;
-var now = new Date();
-
 var fps = 60;
 
-
-
+var now = new Date();
 
 var yearStart = (now.getFullYear() -2003) * yearLength;
 var cameraInitPosition = yearStart + 500;
@@ -32,13 +27,11 @@ var mode = 'auto';
 var start = -yearStart -1000;
 var goal = yearLength * 2;
 var distance = goal - start;
-
 var duration = 30000;
 
 var speed = distance / (duration / 1000 * 60);
 
-
-var scene2, renderer2;
+var linePositionY = -20;
 
 
 $(function(){
@@ -60,26 +53,25 @@ function init() {
 	camera.position.z = 500;
 
 	scene = new THREE.Scene();
-	//scene.fog = new THREE.Fog( 0xefd1b5, 100,2000 );
-	
 	scene2 = new THREE.Scene();
 
 	parent = new THREE.Object3D;
 	parent2 = new THREE.Object3D;
+	
 	scene.add( parent );
 	scene2.add( parent2 );
 	
-
-	//geometryの宣言と生成
 	
+	
+	//TimeLine
 	createline(
-		{x:0, y:-20, z:yearStart},
-		{x:0, y:-20, z:-yearLength / 12 * 7}
+		{x:0, y:linePositionY, z:yearStart},
+		{x:0, y:linePositionY, z: -yearLength / 12 * 5}
 	)
-
 	
-	var wp = $("#wp"), wc = $("#wc")
-	var wpData = json.wp, wcData = json.wc
+	createPoint('l',{x:0, y:linePositionY, z: -yearLength / 12 * 5})
+	
+
 	
 	//Year 
 	var currentYear = now.getFullYear();
@@ -90,58 +82,72 @@ function init() {
 		object.position.x = 0;
 		object.position.y = 0;
 		
-		createPoint('m',{x:0,y:-20,z:getZPosition(i)})
-		
-
-		
+		createPoint('m',{x:0,y:linePositionY,z:getZPosition(i)})
 	}
-	
 
 	//WordPress release date
-	
+	var wp = $("#wp"), wc = $("#wc");
+	var wpData = json.wp, wcData = json.wc
 	
 	for(var i = 0; i < wpData.length; i++) {
-		var src = '<span> v' + wpData[i]. version + '</span>' +'<h2>' + wpData[i].codename + '</h2>' + '<h3>' + wpData[i].date + '</h3>';
+		var date = dateFix(wpData[i].date)
+		var src = '<span class="date">' + date + '<span><h2>v' + wpData[i].version + ' ' +wpData[i].codename + '</h2>';
 		var element = createElement(src);
+		$(element).addClass="wp";
 		var object = createObject(element, wpData[i].date);
 		object.position.x = wpXLength  * Math.cos(60 * i / 180 * Math.PI) + wpXLength *2 ;
 		object.position.y = Math.random() * yRamdom - yRamdom / 2;
 		
-		console.log(object.position.x)
+		createPoint('s',{x:object.position.x,y:object.position.y,z:object.position.z})
+		createPoint('s',{x:0,y:linePositionY,z:object.position.z})
 		
-		createPoint('s',{x:0,y:-20,z:object.position.z})
-		
+	
+
 		createline(
-			{x:object.position.x - 0,y:object.position.y,z:object.position.z},
-			{x:0,y:-20,z:object.position.z}
+			{x:object.position.x - 0,y:object.position.y, z:object.position.z},
+			{x:0,y:linePositionY,z:object.position.z}
 		)
 	}
 	
 	//WordCamp hold date
 	for(var i = 0; i < wcData.length; i++) {
-		var src = '<span>' + wcData[i]. date + '</span>' +'<h2>' + wcData[i].name + '</h2>'
-		var element = createElement(src)
+		var date = dateFix(wcData[i].date)
+		var src = '<span class="date">' + date + '</span>' +'<h2>' + wcData[i].name + '</h2>'
+		if ('wapuu' in wcData[i]) {
+			src += '<img src="img/wapuu/'+ wcData[i].wapuu +'" alt="' + wcData[i].name +'" />'
+		}
+		
+		var element = createElement(src);
+		$(element).addClass('wc');
+		
 		var object = createObject(element, wcData[i].date);
 		object.position.x = wpXLength  * Math.cos(60 * i / 180 * Math.PI) - wpXLength *2 ;
 		object.position.y = Math.random() * yRamdom - yRamdom / 2;
 		
-		createPoint('s',{x:0,y:-20,z:object.position.z})
+		createPoint('s',{x:object.position.x,y:object.position.y,z:object.position.z})
+		createPoint('s',{x:0,y:linePositionY,z:object.position.z})
 		
 		createline(
 			{x:object.position.x,y:object.position.y,z:object.position.z},
-			{x:0,y:-20,z:object.position.z}
+			{x:0,y:linePositionY,z:object.position.z}
 		)
 	}
 	
-	var src = '<span>7/9,10/2016</span>' +'<h1>WordCamp Kansai 2016</h1>';
+	//WCK Kansai 2016
+	var src = '<img class="wapuu wapuu_hover" src="img/wapuu/wapuu_wck_2016_hover.png" />';
+	src += '<img class="wapuu wapuu_headset" src="img/wapuu/wapuu_wck_2016_headset.png" />';
+	src += '<span class="date">July/9,10/2016</span>';
+	src += '<h1 class="logo"><img src="img/wck_2016_logo.svg"></h1>';
 	var element = createElement(src);
 	$(element).addClass('wck2016');
-	var object = createObject(element, '7/9,10/2016');
+	var object = createObject(element, '2017');
 	
 	
 	
+	stats = new Stats();
+	container.appendChild( stats.domElement );
 	
-	renderer = new THREE.CSS3DRenderer();
+	renderer = new THREE.CSS3DRenderer({antialias: true});
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	renderer.domElement.style.position = 'absolute';
 	container.appendChild( renderer.domElement );
@@ -152,14 +158,11 @@ function init() {
 	renderer2.setSize( window.innerWidth, window.innerHeight );
 	container.appendChild( renderer2.domElement );
 	
-	stats = new Stats();
-	container.appendChild( stats.domElement );
-	
+
 	controls = new THREE.TrackballControls( camera, renderer.domElement );
 	controls.rotateSpeed = 2;
 	controls.minDistance = 0;
 	controls.maxDistance = 2000;
-	//controls.addEventListener( 'change', render );
 	
 	window.addEventListener( 'resize', onWindowResize, false );
 	
@@ -203,21 +206,34 @@ function createline(v1, v2) {
 	parent2.add( line );
 }
 
+
+function dateFix(_date) {
+	var split = _date.split('/')
+	var monthNum = parseInt(split[0].replace('0','')) - 1;
+
+	var month = monthNames[monthNum];
+	var date = month + ' / ' + split[1] + ' / ' + split[2];
+	return date;
+
+}
+
+
+
 function getZPosition(date){
 	
 	var currentYear = now.getFullYear() - 2003;
 	var fixPosition = (currentYear - 3) / 2;
 	
 	date = date.toString()
-	var sprit = date.split('/');
+	var split = date.split('/');
 	var position;
 	
-	if(sprit){
-		var year = parseInt(sprit[sprit.length-1]) - 2003;
+	if(split){
+		var year = parseInt(split[split.length-1]) - 2003;
 		
 		
-		if(sprit.length > 1){
-			var month = parseInt(sprit[0]) / 12;
+		if(split.length > 1){
+			var month = parseInt(split[0]) / 12;
 			var dayNumber = 31;
 			if(month == 4 || month == 6 || month == 9 || month == 11){
 				dayNumber = 30;
@@ -228,7 +244,7 @@ function getZPosition(date){
 					dayNumber = 28;
 				}
 			}
-			var day = parseInt(sprit[1]) / dayNumber;
+			var day = parseInt(split[1]) / dayNumber;
 			var dayLength = yearLength / 12 / dayNumber;
 			position = yearStart - yearLength * year - yearLength * month - dayLength;
 		}else{
@@ -239,12 +255,6 @@ function getZPosition(date){
 	return position;
 }
 
-
-function reset() {
-	parent.position.z = start;
-	parent2.position.z = start;
-}
-	
 
 function onWindowResize() {
 	camera.aspect = window.innerWidth / window.innerHeight;
@@ -272,6 +282,11 @@ function animate() {
 function render() {
 	renderer.render( scene, camera );
 	renderer2.render( scene2, camera );
+}
+
+function reset() {
+	parent.position.z = start;
+	parent2.position.z = start;
 }
 
 
